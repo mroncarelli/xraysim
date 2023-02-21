55b7a1955f7ccb716d0502b70d47ab57ed3ab16f
Mauro Roncarelli <mauro.roncarelli@gmail.com>
2023-02-21 11:27:04 +0100

    Minor modification

--- src/make_2d_maps.py
+++ src/make_2d_maps.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 indir = '/Users/mauro/XRISM/TheThreeHundred/Gadget3PESPH/NewMDCLUSTER_0322/'
 infile = 'snap_128'
 
-res, norm = makemap(indir + infile, 'vmw')
+res, norm = makemap(indir + infile, 'rho')
 print(res.min(), res.max())
-#plt.imshow(res, norm=LogNorm(vmin=1.e6, vmax=1.e8))
+plt.imshow(res)
 plt.imshow(res)
 plt.show()
