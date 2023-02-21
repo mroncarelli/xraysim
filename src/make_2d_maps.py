43b25049f2077092a54c9325f8ccb5bd787e8d56
Mauro Roncarelli <mauro.roncarelli@gmail.com>
2023-02-21 11:12:29 +0100

    Several modifications, but something is not working, maps are wrong

--- src/make_2d_maps.py
+++ src/make_2d_maps.py
@@ -6,8 +6,11 @@ import matplotlib.pyplot as plt
 indir = '/Users/mauro/XRISM/TheThreeHundred/Gadget3PESPH/NewMDCLUSTER_0322/'
 infile = 'snap_128'
 
-res, norm = makemap(indir + infile, 'vmw')
-print(res.min(), res.max())
+#res = makemap(indir + infile, 'rho', struct = True, npix=512, center=[500000., 500000.], size=10000., tcut=-1.e5, sample=100)
+res = makemap(indir + infile, 'rho', struct = True, npix=512)
+print(res['map'].min(), res['map'].max())
 #plt.imshow(res, norm=LogNorm(vmin=1.e6, vmax=1.e8))
-plt.imshow(res)
+#plt.imshow(res['map'], norm=LogNorm(vmin=1.e6, vmax=1.e8), extent=[res['xrange'][0], res['xrange'][1], res['yrange'][0], res['yrange'][1]])
+plt.imshow(res['map'].transpose(), origin='lower', extent=[res['xrange'][0], res['xrange'][1], res['yrange'][0], res['yrange'][1]])
 plt.show()
+print('pippo')
--- src/proj2d/makemap.py
+++ src/proj2d/makemap.py
@@ -1,19 +1,23 @@
 import numpy as np
 import pygadgetreader as pygr
+from tqdm import tqdm
 
-from .phys_const import k_B, m_p, Xp, Yp, mu0
-from .linkedlist import linkedlist2d
 from .intkernel import intkernel
+from .linkedlist import linkedlist2d
 
-def makemap(filename: str, qty, center=None, size=None, proj='z', npix=256):
+
+def makemap(filename: str, qty, npix=256, center=None, size=None, proj='z', tcut=0., sample=1, struct=False):
     """
 
     :param filename: (str) input file
     :param qty: (str) quantity to integrate, i.e. Int(qty*dl)
+    :param npix: (int) number of map pixels per side
     :param center: (float 2) comoving coord. of the map center [h^-1 kpc], default: median point of gas particles
     :param size: (float) map comoving size [h^-1 kpc], default: encloses all gas particles
     :param proj: (str/int) direction of projection ('x', 'y', 'z' or 0, 1, 2)
-    :param npix: (int) number of map pixels per side
+    :param tcut: (float) if set defines a temperature cut below which particles are removed [K], default: 0.
+    :param sample: (int), if set defines a sampling for the particles (useful to speed up), default: 1 (no sampling)
+    :param struct: (bool) if set outputs a structure (dictionary) containing several info, default: False TODO: info
     :return:
     """
 
@@ -45,20 +49,20 @@ def makemap(filename: str, qty, center=None, size=None, proj='z', npix=256):
     # Defining center and map size
     hsml = pygr.readsnap(filename, 'hsml', 'gas', units=0)  # [h^-1 kpc] comoving
     if center is None:
-        xMin, xMax = min(x - hsml), max(x + hsml)
-        yMin, yMax = min(y - hsml), max(y + hsml)
-        xc = 0.5 * (xMin + xMax)
-        yc = 0.5 * (yMin + yMax)
+        xmin, xmax = min(x - hsml), max(x + hsml)
+        ymin, ymax = min(y - hsml), max(y + hsml)
+        xc = 0.5 * (xmin + xmax)
+        yc = 0.5 * (ymin + ymax)
         if size is None:
-            deltaX, deltaY = xMax - xMin, yMax - yMin
+            deltaX, deltaY = xmax - xmin, ymax - ymin
             if deltaX >= deltaY:
                 size = deltaX
-                x0, y0 = xMin, yMin - 0.5 * (deltaX - deltaY)
+                xmap0, ymap0 = xmin, ymin - 0.5 * (deltaX - deltaY)
             else:
                 size = deltaY
-                x0, y0 = xMin - 0.5 * (deltaY - deltaX), yMin
+                xmap0, ymap0 = xmin - 0.5 * (deltaY - deltaX), ymin
         else:
-            x0, y0 = xc - 0.5 * size, yc - 0.5 * size
+            xmap0, ymap0 = xc - 0.5 * size, yc - 0.5 * size
     else:
         try:
             xc, yc = float(center[0]), float(center[1])
@@ -67,64 +71,67 @@ def makemap(filename: str, qty, center=None, size=None, proj='z', npix=256):
             raise ValueError
 
         if size is None:
-            xMin, xMax = min(x - hsml), max(x + hsml)
-            yMin, yMax = min(y - hsml), max(y + hsml)
-            if not (xMin <= xc <= xMax and yMin <= yc <= yMax):
+            xmin, xmax = min(x - hsml), max(x + hsml)
+            ymin, ymax = min(y - hsml), max(y + hsml)
+            if not (xmin <= xc <= xmax and ymin <= yc <= ymax):
                 print("WARNING: Map center is outside the simulation box")
-                size = max(abs(xc - xMin), abs(xc - xMax), abs(yc - yMin), abs(yc - yMax))
+            size = 2. * max(abs(xc - xmin), abs(xc - xmax), abs(yc - ymin), abs(yc - ymax))
 
-        x0, y0 = xc - 0.5 * size, yc - 0.5 * size
+        xmap0, ymap0 = xc - 0.5 * size, yc - 0.5 * size
 
-    # Normalizing coordinates in pixel units (between 0 and nPix)
-    x = (x - x0) / size * npix
-    y = (y - y0) / size * npix
+    # Normalizing coordinates in pixel units (0 = left/bottom border, npix = right/top border)
+    x = (x - xmap0) / size * npix
+    y = (y - ymap0) / size * npix
     hsml = hsml / size * npix
-    pixSize = size / npix  # comoving [h^-1 kpc]
-    pixSize_physical = pixSize / (1 + redshift)  # physical [h^-1 kpc]
+    pixsize = size / npix  # comoving [h^-1 kpc]
 
-    # Create linked lists
-    lkdlist_first, lkdlist_next = linkedlist2d(x, y, npix, npix)
+    # Create linked list and cutting out particles
+    if tcut > 0.:
+        temp = pygr.readsnap(filename, 'u', 'gas', units=1)  # [K]
+        index_to_map = np.where((x + hsml > 0) & (x - hsml < npix) & (y + hsml > 0) & (y - hsml < npix) & (temp > tcut))[0]
+        if qty not in ['Tmw', 'Tew', 'Tsl']:
+            del temp
+    else:
+        index_to_map = np.where((x + hsml > 0) & (x - hsml < npix) & (y + hsml > 0) & (y - hsml < npix))[0]
+    particle_list = index_to_map[linkedlist2d(x[index_to_map], y[index_to_map], npix, npix)]
+    del index_to_map
 
     # Calculating quantity (q) to integrate and weight (w)
     mass = pygr.readsnap(filename, 'mass', 'gas', units=0)  # [10^10 h^-1 M_Sun]
     if qty == 'rho':  # Int(rho*dl)
-        q = mass  # [10^10 h^-1 M_Sun]
-        w = np.full(ngas, 1.)
+        q = mass / pixsize ** 2  # comoving [10^10 h M_Sun kpc^-2]
+        w = np.full(ngas, 1.)  # [---]
     elif qty == 'rho2':  # Int(rho2*dl)
-        q = mass * pygr.readsnap(filename, 'rho', 'gas', units=0)  # [10^20 h M_Sun^2 kpc^-3]
-        w = np.full(ngas, 1.)
+        q = mass * pygr.readsnap(filename, 'rho', 'gas', units=0) / pixsize ** 2  # comoving [10^20 h^3 M_Sun^2 kpc^-1]
+        w = np.full(ngas, 1.)  # [---]
     elif qty in ['Tmw', 'Tew', 'Tsl']:
-        u = pygr.readsnap(filename, 'u', 'gas', units=0)  # internal energy per unit mass [km^2 s^-2]
-        if f_cooling == 0:
-            temp = 2. / 3. * u * (1.e5 ** 2) * mu0 * m_p / k_B  # [K] (full ionization)
-        else:
-            ne = pygr.readsnap(filename, 'ne', 'gas', units=0)
-            temp = 2. / 3. * u * (1.e5 ** 2) / ((1. + ne) * Xp + 0.25 * Yp) * m_p / k_B  # [K]
+        if not 'temp' in locals():
+            temp = pygr.readsnap(filename, 'u', 'gas', units=1)  # internal energy per unit mass [km^2 s^-2]
 
         if qty == 'Tmw':
             q = mass * temp  # [10^10 h^-1 M_Sun K]
-            w = mass  # [10^10 h^-1 M_Sun]
+            w = mass / pixsize ** 2  # comoving [10^10 h M_Sun kpc^-2]
         elif qty == 'Tew':
             rho = pygr.readsnap(filename, 'rho', 'gas', units=0)  # [10^10 h^2 M_Sun kpc^-3]
             q = mass * rho * temp  # [10^20 h M_Sun^2 kpc^-3 K]
-            w = mass * rho  # [10^20 h M_Sun^2 kpc^-3]
+            w = mass * rho / pixsize ** 2  # [10^20 h^3 M_Sun^2 kpc^-5]
             del rho
         elif qty == 'Tsl':
             rho = pygr.readsnap(filename, 'rho', 'gas', units=0)  # [10^10 h^2 M_Sun kpc^-3]
             q = mass * rho * temp ** 0.25  # [10^20 h M_Sun^2 kpc^-3 K^0.25]
-            w = mass * rho * temp ** (-0.75)  # [10^20 h M_Sun^2 kpc^-3 K^-0.75]
+            w = mass * rho * temp ** (-0.75) / pixsize ** 2  # [10^20 h^3 M_Sun^2 kpc^-5 K^-0.75]
             del rho
         del mass, temp
 
     elif qty in ['vmw', 'vew']:
         vel = pygr.readsnap(filename, 'vel', 'gas', units=0)[:, projInd] / (1 + redshift)  # [km s^-1]
         if qty == 'vmw':
-            q = mass * vel  # [10^10 h^-1 M_Sun km s^-1]
-            w = mass  # [10^10 h^-1 M_Sun]
+            q = mass * vel / pixsize ** 2  # [10^10 h M_Sun kpc^-2 km s^-1]
+            w = mass / pixsize ** 2  # [10^10 h M_Sun kpc^-2]
         elif qty == 'vew':
             rho = pygr.readsnap(filename, 'rho', 'gas', units=0)  # [10^10 h^2 M_Sun kpc^-3]
-            q = mass * rho * vel  # [10^20 h M_Sun^2 kpc^-3 km s^-1]
-            w = mass * rho  # [10^20 h M_Sun^2 kpc^-3]
+            q = mass * rho * vel / pixsize ** 2  # [10^20 h^3 M_Sun^2 kpc^-5 km s^-1]
+            w = mass * rho / pixsize ** 2  # [10^20 h^3 M_Sun^2 kpc^-5]
             del rho
         del mass, vel
     else:
@@ -135,33 +142,58 @@ def makemap(filename: str, qty, center=None, size=None, proj='z', npix=256):
     qmap = np.full((npix, npix), 0.)
     wmap = np.full((npix, npix), 0.)
 
-    ipart = lkdlist_first
-    while ipart != -1:
+    for ipart in tqdm(particle_list[::sample]):
 
         step = 1. / hsml[ipart]  # 1 pixel-shift in units of hsml
 
         i_beg = max(int(np.floor(x[ipart] - hsml[ipart])), 0)
-        i_end = min(int(np.ceil(x[ipart] + hsml[ipart])), npix - 1)
+        i_end = min(int(np.ceil(x[ipart] + hsml[ipart])), npix)
         j_beg = max(int(np.floor(y[ipart] - hsml[ipart])), 0)
-        j_end = min(int(np.ceil(y[ipart] + hsml[ipart])), npix - 1)
+        j_end = min(int(np.ceil(y[ipart] + hsml[ipart])), npix)
         x0 = (i_beg - x[ipart]) * step
         wkx0 = intkernel(x0)
         for imap in range(i_beg, i_end):
             x1 = x0 + step
             wkx1 = intkernel(x1)
-            wkx = wkx1-wkx0
+            wkx = wkx1 - wkx0
             y0 = (j_beg - y[ipart]) * step
             wky0 = intkernel(y0)
             for jmap in range(j_beg, j_end):
                 y1 = y0 + step
                 wky1 = intkernel(y1)
-                wk = (wky1-wky0) * wkx
+                wk = (wky1 - wky0) * wkx
                 qmap[imap, jmap] += q[ipart] * wk
                 wmap[imap, jmap] += w[ipart] * wk
                 y0, wky0 = y1, wky1
             x0, wkx0 = x1, wkx1
 
-        ipart = lkdlist_next[ipart]
-
     qmap[np.where(wmap != 0.)] /= wmap[np.where(wmap != 0.)]
-    return qmap, wmap
+
+    # Output
+    if struct:
+
+        units = {
+            'rho': {'map': '10^10 h M_Sun kpc^-2', 'norm': '---'},
+            'rho2': {'map': '10^20 h^3 M_Sun^2 kpc^-1', 'norm': '---'},
+            'Tmw': {'map': 'K', 'norm': '10^10 h M_Sun kpc^-2'},
+            'Tew': {'map': 'K', 'norm': '10^20 h^3 M_Sun^2 kpc^-5'},
+            'Tsl': {'map': 'K', 'norm': '10^20 h^3 M_Sun^2 kpc^-5 K^-0.75'},
+            'vmw': {'map': 'km s^-1', 'norm': '10^10 h M_Sun kpc^-2'},
+            'vew': {'map': 'km s^-1', 'norm': '10^20 h^3 M_Sun^2 kpc^-5'}
+        }
+
+        result = {
+            'map': qmap,
+            'norm': wmap,
+            'xrange': (xmap0, xmap0 + size),  # [h^-1 kpc] comoving
+            'yrange': (ymap0, ymap0 + size),  # [h^-1 kpc] comoving
+            'units': units[qty]['map'],
+            'norm_units': units[qty]['norm'],
+            'coord_units': 'h^-1 kpc'
+        }
+
+        return result
+
+    else:
+
+        return qmap
