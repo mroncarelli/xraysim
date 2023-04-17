1d85b5a9c25c08ac31c898587184969753c0def8
Mauro Roncarelli <mauro.roncarelli@gmail.com>
2023-04-17 16:26:19 +0200

    Minor modifications

--- makeit.sh
+++ makeit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./clean.sh
-/usr/local/bin/python3 setup.py build_ext --inplace
-/usr/local/bin/python3 setup.py install
+python setup.py build_ext --inplace
+python setup.py install
--- testit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/Library/Frameworks/Python.framework/Versions/3.11/bin/pytest
