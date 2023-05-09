439030f2c35c65637a0ee98101b3a32323944af5
Mauro Roncarelli <mauro.roncarelli@gmail.com>
2023-05-09 17:26:23 +0200

    Added small emission table to use for tests

--- tests/test_calc_spec.py
+++ tests/test_calc_spec.py
@@ -2,13 +2,13 @@ import os
 import pytest
 from specutils.tables import read_spectable, calc_spec
 
-spectable_file = os.environ.get('XRAYSIM') + '/tests/data/emission_table.fits'
+spectable_file = os.environ.get('XRAYSIM') + '/tests/data/test_emission_table.fits'
 
 
 def test_table_values():
     # A spectrum computed at a z and temperature that match table values must return the values of the table
     spec_table = read_spectable(spectable_file)
-    iz = 10
+    iz = 4
     it = 20
     spec = calc_spec(spec_table, spec_table.get('z')[iz], spec_table.get('temperature')[it])
     assert all(v / v_table == pytest.approx(1) for v, v_table in zip(spec, spec_table.get('data')[iz, it, :]))
