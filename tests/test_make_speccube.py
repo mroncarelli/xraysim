4da2220c9078e73d37ed2f8fb93ef0d3241ffbe1
Mauro Roncarelli <mauro.roncarelli@gmail.com>
2023-09-06 12:12:17 +0200

    Added reference speccube and Simput files for testing. Added test for speccube file that checks each saved value.

--- tests/test_make_speccube.py
+++ tests/test_make_speccube.py
@@ -3,17 +3,18 @@ import numpy as np
 import os
 from astropy.io import fits
 
-from src.pkg.sphprojection.mapping import make_speccube
+from src.pkg.sphprojection.mapping import make_speccube, write_speccube
 from src.pkg.gadgetutils.phys_const import keV2K
 from src.pkg.specutils.tables import read_spectable, calc_spec
 
-snapshot_file = os.environ.get('XRAYSIM') + '/tests/data/snap_Gadget_sample'
-spfile = os.environ.get('XRAYSIM') + '/tests/data/test_emission_table.fits'
-npix, size, redshift, center, proj, flag_ene, nsample = 10, 1., 0.1, [2500., 2500.], 'z', False, 1
+data_dir = os.environ.get('XRAYSIM') + '/tests/data/'
+snapshot_file = data_dir + 'snap_Gadget_sample'
+spfile = data_dir + 'test_emission_table.fits'
+reference_file = data_dir + 'reference.speccube'
+npix, size, redshift, center, proj, flag_ene, nsample, nh = 25, 1.05, 0.1, [2500., 2500.], 'z', False, 1, 0.01
 nene = fits.open(spfile)[0].header.get('NENE')
 
-spec_cube = make_speccube(snapshot_file, spfile, size=size, npix=npix, redshift=0.1, center=center, proj=proj,
-                          nsample=nsample)
+spec_cube = make_speccube(snapshot_file, spfile, size=size, npix=npix, redshift=0.1, nh=nh, center=center, proj=proj)
 
 
 def test_structure(inp=spec_cube):
@@ -115,3 +116,46 @@ def test_isothermal_spectrum():
 
     for val, val_reference in zip(spec_iso, spec_reference):
         assert val / val_reference == pytest.approx(1., rel=1.e-5)
+
+
+def test_file_reference(reference=reference_file):
+    """
+    Writing the spec_cube to a fits file should produce a file with data identical to the reference one.
+    """
+    test_file = data_dir + 'file_created_for_test.speccube'
+    if os.path.isfile(test_file):
+        os.remove(test_file)
+    write_speccube(spec_cube, test_file)
+    hdulist_reference = fits.open(reference)
+    hdulist = fits.open(test_file)
+
+    # File must have Primary and 2 extensions
+    assert len(hdulist) == len(hdulist_reference) == 3
+
+    # Primary
+    data = hdulist[0].data
+    data_reference = hdulist_reference[0].data
+    assert len(data.shape) == len(data_reference.shape) == 3
+    assert data.shape == data_reference.shape
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            for k in range(data.shape[2]):
+                assert data[i, j, k] == pytest.approx(data_reference[i, j, k])
+
+    # Extension 1
+    data = hdulist[1].data
+    data_reference = hdulist_reference[1].data
+    assert len(data.shape) == len(data_reference.shape) == 1
+    assert data.shape == data_reference.shape
+    for i in range(data.shape[0]):
+        assert data[i] == pytest.approx(data_reference[i])
+
+    # Extension 2
+    data = hdulist[2].data
+    data_reference = hdulist_reference[2].data
+    assert len(data.shape) == len(data_reference.shape) == 1
+    assert data.shape == data_reference.shape
+    for i in range(data.shape[0]):
+        assert data[i] == pytest.approx(data_reference[i])
+
+    os.remove(test_file)
