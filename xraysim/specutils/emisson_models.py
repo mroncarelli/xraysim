e49e29940edd849b7b7b0c771202e8f3b6a8b36f
atulit-pc <srivastava.atulit@gmail.com>
2023-11-17 14:25:11 +0100

    emission model apec, vvapec

--- xraysim/specutils/emisson_models.py
+++ xraysim/specutils/emisson_models.py
@@ -5,9 +5,6 @@ import numpy as np
 import xspec as xsp
 
 
-# from gadgetutils.convert import gadgget2xspecnorm
-
-
 class EmissionModels:
     def __init__(self, model_name: str, energy: np.ndarray):
         models_config_file = os.path.join(os.path.dirname(__file__), 'em_reference.json')
@@ -50,11 +47,11 @@ class EmissionModels:
             met_species = np.array([1, 2, 6, 7, 8, 10, 12, 14, 16, 20, 26]) - 1
             self.json_record['metals_ref'][met_species] = metal
 
-    def calculate_spectrum(self, z, temperature, metallicity, flag_ene=False):
+    def calculate_spectrum(self, z, temperature, metallicity, norm, flag_ene=False):
 
         self.set_metals_ref(metallicity)
 
-        params = [temperature, *(self.json_record['metals_ref']).tolist(), z, 1E-14]
+        params = [temperature, *(self.json_record['metals_ref']).tolist(), z, norm]
         self.model.setPars(params)
         self.model.show()
         result = self.model.values(0)
@@ -65,16 +62,23 @@ class EmissionModels:
         return result
 
 
-# Testing Line For Checking The Class :
+# Testing Line For Checking The Class and setup :
+
+from gadgetutils.convert import gadgget2xspecnorm
+
+# gad
+#
+
+xspec_norm = 1E-14
+
 a = EmissionModels('TheThreeHundred-1', np.linspace(0.1, 10, 1000))
 
-print(a.calculate_spectrum(0.1, 0.34, [0.04], False))
-print(a.calculate_spectrum(.2, 0.6, [0.01], False))
-print(a.calculate_spectrum(.2, 0.2, [0.07], False))
-
+print(a.calculate_spectrum(0.1, 0.34, [0.04], xspec_norm, False))
+print(a.calculate_spectrum(.2, 0.6, [0.01], xspec_norm, False))
+print(a.calculate_spectrum(.2, 0.2, [0.07], xspec_norm, False))
 
 b = EmissionModels('TheThreeHundred-3', np.linspace(0.1, 10, 1000))
 
-print(a.calculate_spectrum(0.1, 0.34, np.linspace(0.1, 0.3, 11), False))
-print(a.calculate_spectrum(.2, 0.6, np.linspace(0.2, 0.3, 11), False))
-print(a.calculate_spectrum(.2, 0.2, np.linspace(0.4, 0.5, 11), False))
+print(b.calculate_spectrum(0.1, 0.34, np.linspace(0.1, 0.3, 11), xspec_norm, False))
+print(b.calculate_spectrum(.2, 0.6, np.linspace(0.2, 0.3, 11), xspec_norm, False))
+print(b.calculate_spectrum(.2, 0.2, np.linspace(0.4, 0.5, 11), xspec_norm, False))
