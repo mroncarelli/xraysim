ff78c7fa8764ad4c5652169e55fa40b80dfed896
atulit-pc <srivastava.atulit@gmail.com>
2023-11-30 22:55:52 +0100

    emission model apec, vvapec flexibility and choice to use : pyatomdb or pyxspec

--- xraysim/specutils/emisson_models.py
+++ xraysim/specutils/emisson_models.py
@@ -6,10 +6,57 @@ import xspec as xsp
 import pyatomdb
 import matplotlib.pyplot as plt
 
-
 models_config_file = os.path.join(os.path.dirname(__file__), 'em_reference.json')
 with open(models_config_file) as file:
     json_data = json.load(file)
+
+
+class XspecModel:
+    def __init__(self, model_name, energy):
+        xsp.AllModels.setEnergies(f"{energy.min()} {energy.max()} {len(energy) - 1} lin")
+        xsp.Xset.addModelString("APECROOT", "3.0.9")
+        self.xspec_model = xsp.Model(model_name)
+
+    # doesn't change the object itself, that's why we have this warning
+    def set_xspec_commands(self, commands):
+        xspec_settings = {
+            'abund': lambda cmd: setattr(xsp.Xset, cmd['method'], cmd['arg']),
+            'addModelString': lambda cmd: xsp.Xset.addModelString(cmd['arg'][0], cmd['arg'][1]),
+        }
+
+        for command in commands:
+            xspec_settings.get(command['method'], lambda cmd: None)(command)
+
+    def calculate_spectrum(self, z, temperature, elements_index, metallicity, norm):
+        params = [temperature, *metallicity.tolist(), z, norm]
+        self.xspec_model.setPars(params)
+        self.xspec_model.show()
+        result = self.xspec_model.values(0)
+
+        return result
+
+
+class AtomdbModel:
+
+    def __init__(self, model_name, energy):
+        self.atomdb_model = pyatomdb.spectrum.CIESession()
+        self.energy = energy
+
+    def set_atomdb_commands(self, commands):
+        atomdb_settings = {
+            'abundset': lambda cmd: setattr(self.atomdb_model, cmd['method'], cmd['arg']),
+            'do_eebrems': lambda cmd: setattr(self.atomdb_model, cmd['method'], cmd['arg']),
+            'set_broadening': lambda cmd: self.atomdb_model.set_broadening(thermal_broadening=True)
+        }
+        for command in commands:
+            atomdb_settings.get(command['method'], lambda cmd: None)(command)
+
+    def calculate_spectrum(self, z, temperature, elements_index, metallicity, norm):
+        self.atomdb_model.set_response(self.energy * (1 + z), raw=True)
+        self.atomdb_model.set_abund(elements_index + 1, metallicity[elements_index])
+        result = self.atomdb_model.return_spectrum(temperature, log_interp=False) * norm * (1 / (1 + z)) ** 1
+
+        return result
 
 
 class EmissionModels:
@@ -24,27 +71,15 @@ class EmissionModels:
         self.energy = energy
 
         if self.json_record['code'] == 'xspec':
-            xsp.AllModels.setEnergies(f"{self.energy.min()} {self.energy.max()} {len(self.energy) - 1} lin")
-            xsp.Xset.addModelString("APECROOT", "3.0.9")
-            self.model = xsp.Model(self.json_record['model'])
+            self.model = XspecModel(self.json_record['model'], self.energy)
+            self.model.set_xspec_commands(self.json_record['xset'])
 
         elif self.json_record['code'] == 'atomdb':
-            self.model = pyatomdb.spectrum.CIESession()
+            self.model = AtomdbModel(self.json_record['model'], self.energy)
+            self.model.set_atomdb_commands(self.json_record['xset'])
 
-        self.set_commands()
-
-    def set_commands(self):
-
-        init_settings = {
-            'abund': lambda cmd: setattr(xsp.Xset, cmd['method'], cmd['arg']),
-            'addModelString': lambda cmd: xsp.Xset.addModelString(cmd['arg'][0], cmd['arg'][1]),
-            'abundset': lambda cmd: setattr(self.model, cmd['method'], cmd['arg']),
-            'do_eebrems': lambda cmd: setattr(self.model, cmd['method'], cmd['arg']),
-            'set_broadening': lambda cmd: self.model.set_broadening(thermal_broadening=True)
-        }
-
-        for command in self.json_record['xset']:
-            init_settings.get(command['method'], lambda cmd: None)(command)
+        else:
+            raise ValueError(f"Library '{self.json_record['code']}' not supported.")
 
     def set_metals_ref(self, metal):
 
@@ -59,23 +94,13 @@ class EmissionModels:
 
         return idx
 
-    def calculate_spectrum(self, z, temperature, metallicity, norm, flag_ene=False):
+    def compute_spectrum(self, z, temperature, metallicity, norm, flag_ene=False):
 
-        elements_index = self.set_metals_ref(metallicity)
+        chem_species_index = self.set_metals_ref(metallicity)
 
-        if self.json_record['code'] == 'xspec':
-            params = [temperature, *(self.json_record['metals_ref']).tolist(), z, norm]
-            self.model.setPars(params)
-            self.model.show()
-            result = self.model.values(0)
-
-        elif self.json_record['code'] == 'atomdb':
-            self.model.set_response(self.energy*(1+z), raw=True)
-            self.model.set_abund(elements_index+1,self.json_record['metals_ref'][elements_index])
-            result = self.model.return_spectrum(temperature, log_interp=False)*norm * (1/(1+z))**1
-
+        result = self.model.calculate_spectrum(z, temperature, chem_species_index, self.json_record['metals_ref'], norm)
         if flag_ene:
-            bins = 0.5 * (np.array(self.model.energies(0))[1:] + np.array(self.model.energies(0))[:-1])
+            bins = 0.5 * (np.array(self.energy[1:] + np.array(self.energy)[:-1]))
             result = result * bins
 
         return result
@@ -84,35 +109,28 @@ class EmissionModels:
 # Testing Line For Checking The Class and setup :
 
 xspec_norm = 1
-pyatomdb_norm= 1E14
+pyatomdb_norm = 1
 
-# a = EmissionModels('TheThreeHundred-1', np.linspace(0.1, 10, 1000))
+a = EmissionModels('TheThreeHundred-1', np.linspace(0.1, 10, 1000))
 
-# print(a.calculate_spectrum(0.1, 0.34, [0.04], xspec_norm, False))
-# print(a.calculate_spectrum(.2, 0.6, [0.01], xspec_norm, False))
-# print(a.calculate_spectrum(.2, 0.2, [0.07], xspec_norm, False))
+print(a.compute_spectrum(0.1, 0.34, [0.04], xspec_norm, False))
+print(a.compute_spectrum(.2, 0.6, [0.01], xspec_norm, False))
+print(a.compute_spectrum(.2, 0.2, [0.07], xspec_norm, False))
 
 b = EmissionModels('TheThreeHundred-2', np.linspace(0.1, 10, 1000))
 
-xspec1 = (b.calculate_spectrum(0.1, 3, [0.05], xspec_norm, False))
-# print(b.calculate_spectrum(.2, 0.6, np.linspace(0.2, 0.3, 11), xspec_norm, False))
-# print(b.calculate_spectrum(.2, 0.2, np.linspace(0.4, 0.5, 11), xspec_norm, False))
-
+print(b.compute_spectrum(0.1, 3, [0.05], xspec_norm, False))
+print(b.compute_spectrum(.2, 0.6, np.linspace(0.2, 0.3, 11), xspec_norm, False))
+print(b.compute_spectrum(.2, 0.2, np.linspace(0.4, 0.5, 11), xspec_norm, False))
 
 b = EmissionModels('TheThreeHundred-4', np.linspace(0.1, 10, 1000))
 
-pydb1 = b.calculate_spectrum(0.1, 3, [0.05], pyatomdb_norm, False)
+print(b.compute_spectrum(0.1, 3, [0.05], pyatomdb_norm, False))
+
+# print(pydb1)
+# print(xspec1)
 
 
-print(pydb1)
-print(xspec1)
-
-plt.plot(0.5*(np.linspace(0.1, 10, 1000)[1:]+np.linspace(0.1, 10, 1000)[:-1]),xspec1)
-plt.plot(0.5*(np.linspace(0.1, 10, 1000)[1:]+np.linspace(0.1, 10, 1000)[:-1]),pydb1)
-
-plt.xscale('log')
-plt.yscale('log')
-plt.show()
 # For actual sim
 
 # from gadgetutils.convert import gadgget2xspecnorm
@@ -124,5 +142,5 @@ plt.show()
 # print(mass)
 # gad
 
-#a = EmissionModels('TheThreeHundred-4', np.linspace(4, 5, 1000))
-#print(a.calculate_spectrum(0.1, 0.54, [0.4], xspec_norm, False))
+# a = EmissionModels('TheThreeHundred-4', np.linspace(4, 5, 1000))
+# print(a.calculate_spectrum(0.1, 0.54, [0.4], xspec_norm, False))
