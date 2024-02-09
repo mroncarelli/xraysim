605d9e0182796807fb5f0cb56d80825921b629a1
Mauro Roncarelli <mauro.roncarelli@gmail.com>
2024-02-09 18:22:09 +0100

    Added test to check the presence of the xspec package. Added fit module (empty) in specutils.

--- tests/test_system_requirements.py
+++ tests/test_system_requirements.py
@@ -1,8 +1,10 @@
-from distutils.spawn import find_executable
+from importlib.util import find_spec
 import os
+from distutils.spawn import find_executable
 
 commandList = ['xifupipeline', 'makespec']
 envVariableList = ['XRAYSIM', 'SIXTE_INSTRUMENTS']
+packageList = ['xspec']
 
 
 def test_programs_are_installed():
@@ -20,3 +22,12 @@ def test_environment_variables_are_set():
     """
     for env_var in envVariableList:
         assert os.environ.get(env_var) is not None
+
+
+def test_packages_are_present():
+    """
+    Tests that the necessary packages are present.
+    """
+    for package in packageList:
+        dummy = find_spec(package)
+        assert dummy is not None
